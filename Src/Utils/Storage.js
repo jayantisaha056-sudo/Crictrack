2868628e46e694938b1127a83d1b7945018886fa
jayantisaha056-sudo <jayantisaha056@gmail.com>
2025-09-21 14:55:08 +0530

    Delete Src directory

--- Src/Utils/Storage.js
+++
@@ -1,4 +0,0 @@
-export function loadData(){
-  try{ return JSON.parse(localStorage.getItem('cricktrack_v2')||'{}') }catch(e){ return {} }
-}
-export function saveData(data){ localStorage.setItem('cricktrack_v2', JSON.stringify(data)) }
